125e7eaf5780d0d8d6d6d1d486617766bb1ab543
yuiti <yuiti.usp@gmail.com>
2018-06-12 19:37:28 -0300

    fix payloads

--- app/survey.py
+++ app/survey.py
@@ -9,6 +9,177 @@ api = Api(bp_survey, doc='/swagger/')
 dao = DataAccessObject()
 
 
+payload = {
+    'questions': [
+        {
+            'id': '1',
+            'text': 'Estou apropriadamente envolvido em decisões que afetam meu trabalho.',
+            'type': 'radio-string',
+            'answers': [
+                {
+                    'id': 'sadas979gsa9g6',
+                    'text': 'Discordo Totalmente',
+                    'score': -2
+                },
+                {
+                    'id': '3423246',
+                    'text': 'Discordo',
+                    'score': -1
+                },
+                {
+                    'id': '345hkjfhsdf',
+                    'text': 'Neutro',
+                    'score': 0
+                },
+                {
+                    'id': 'sdb7fd876g',
+                    'text': 'Concordo',
+                    'score': 1
+                },
+                {
+                    'id': 'bvcbcvb098096',
+                    'text': 'Concordo Plenamente',
+                    'score': 2
+                }
+            ],
+            'answered': None
+        },
+        {
+            'id': '2',
+            'text': 'Tenho orgulho do que minha empresa representa.',
+            'type': 'radio-string',
+            'answers': [
+                {
+                    'id': 'sadas979gsa9g6',
+                    'text': 'Discordo Totalmente',
+                    'score': -2
+                },
+                {
+                    'id': '3423246',
+                    'text': 'Discordo',
+                    'score': -1
+                },
+                {
+                    'id': '345hkjfhsdf',
+                    'text': 'Neutro',
+                    'score': 0
+                },
+                {
+                    'id': 'sdb7fd876g',
+                    'text': 'Concordo',
+                    'score': 1
+                },
+                {
+                    'id': 'bvcbcvb098096',
+                    'text': 'Concordo Plenamente',
+                    'score': 2
+                }
+            ],
+            'answered': None
+        },
+        {
+            'id': '3',
+            'text': 'Sinto que meu trabalho tem um propósito.',
+            'type': 'radio-string',
+            'answers': [
+                {
+                    'id': 'sadas979gsa9g6',
+                    'text': 'Discordo Totalmente',
+                    'score': -2
+                },
+                {
+                    'id': '3423246',
+                    'text': 'Discordo',
+                    'score': -1
+                },
+                {
+                    'id': '345hkjfhsdf',
+                    'text': 'Neutro',
+                    'score': 0
+                },
+                {
+                    'id': 'sdb7fd876g',
+                    'text': 'Concordo',
+                    'score': 1
+                },
+                {
+                    'id': 'bvcbcvb098096',
+                    'text': 'Concordo Plenamente',
+                    'score': 2
+                }
+            ],
+            'answered': None
+        },
+        {
+            'id': '4',
+            'text': 'Eu me considero um promotor da empresa.',
+            'type': 'radio-string',
+            'answers': [
+                {
+                    'id': 'sadas979gsa9g6',
+                    'text': 'Discordo Totalmente',
+                    'score': -2
+                },
+                {
+                    'id': '3423246',
+                    'text': 'Discordo',
+                    'score': -1
+                },
+                {
+                    'id': '345hkjfhsdf',
+                    'text': 'Neutro',
+                    'score': 0
+                },
+                {
+                    'id': 'sdb7fd876g',
+                    'text': 'Concordo',
+                    'score': 1
+                },
+                {
+                    'id': 'bvcbcvb098096',
+                    'text': 'Concordo Plenamente',
+                    'score': 2
+                }
+            ],
+            'answered': None
+        },
+        {
+            'id': '5',
+            'text': 'Minha empresa celebra nossas conquistas e aprendizados.',
+            'type': 'radio-string',
+            'answers': [
+                {
+                    'id': 'sadas979gsa9g6',
+                    'text': 'Discordo Totalmente',
+                    'score': -2
+                },
+                {
+                    'id': '3423246',
+                    'text': 'Discordo',
+                    'score': -1
+                },
+                {
+                    'id': '345hkjfhsdf',
+                    'text': 'Neutro',
+                    'score': 0
+                },
+                {
+                    'id': 'sdb7fd876g',
+                    'text': 'Concordo',
+                    'score': 1
+                },
+                {
+                    'id': 'bvcbcvb098096',
+                    'text': 'Concordo Plenamente',
+                    'score': 2
+                }
+            ],
+            'answered': None
+        }
+    ]
+}
+
+
 @bp_survey.record_once
 def on_registration(state):
     dao.init_app(state.app.config)
@@ -23,7 +194,7 @@ class Survey(Resource):
             'profile': 'test_profile',
             'role': 'test_role',
         }
-        return dao.find_temp()
+        return payload
 
     def post(self):
-        return dao.find_temp()
+        return payload
