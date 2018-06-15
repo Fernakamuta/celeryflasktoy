4f67343a8bd9c4ecb0b5046b17a846b06c5bfe79
Nakamuta <fernando.nakamuta@sciensa.com>
2018-06-15 16:18:29 -0300

    feat: add test script to build & update requirements.txt

---
+++ tests/test_schemas.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+from app.schemas import get_language, validate_token, QuestionsSchema
+
+
+
+class TestSchema:
+    @pytest.mark.parametrize('lang_in, lang_out', [
+        (None, 'pt-br'),
+        ('fr-be', 'pt-br'),
+        ('ja-jp', 'pt-br'),
+        ('es-sp', 'es-ar'),
+        ('es-ar', 'es-ar'),
+    ])
+    def test_get_language(self, lang_in, lang_out):
+        assert get_language(lang_in) == lang_out
